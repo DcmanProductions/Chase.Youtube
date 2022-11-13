1e7d7190408b03e1a846b7515522ad6aa4fa1be2
Drew Chase <dcmanproductions@gmail.com>
2022-11-13 12:04:47 -0500

    Add project files.

---
+++ Chase.Youtube/YTUrlParser.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Text.RegularExpressions;
+
+namespace Chase.Youtube;
+
+public static class YTUrlParser
+{
+
+    public static string GetDirectUrl(string key)
+    {
+        return "";
+    }
+
+    private static string GetHTML(string key)
+    {
+        string html = "";
+        string url = $"https://youtube.com/watch?v={key}";
+        using (HttpClient client = new())
+        {
+            using HttpResponseMessage response = client.GetAsync(url).Result;
+            if (response != null && response.IsSuccessStatusCode)
+            {
+                html = response.Content.ReadAsStringAsync().Result;
+            }
+        }
+
+        string json = Regex.Match(html, "(?<=(var ytInitialPlayerResponse = ))((?!\\};).)*.").Value;
+        return json;
+    }
+
+}
